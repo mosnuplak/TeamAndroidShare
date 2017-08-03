900335a784d4784fc1f9f43af4166ef20735fbfb
E5-473G <nattachai_prom@hotmail.com>
2017-08-03 15:59:30 +0700

    Initial commit

---
+++ app/src/main/java/com/mfec/teamandroidshare/MainApplication.java
@@ -0,0 +1,22 @@
+package com.mfec.teamandroidshare;
+
+import android.app.Application;
+
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+/**
+ * Created by E5-473G on 8/3/2017.
+ */
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Contextor.getInstance().init(getApplicationContext());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
