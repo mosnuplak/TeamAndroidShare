caa5b504a722bf058b55b935c3381a284e02432c
E5-473G <nattachai_prom@hotmail.com>
2017-08-03 16:23:38 +0700

    setText by mos

--- app/src/main/java/com/mfec/teamandroidshare/activity/MainActivity.java
+++ app/src/main/java/com/mfec/teamandroidshare/activity/MainActivity.java
@@ -2,14 +2,18 @@ package com.mfec.teamandroidshare.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.mfec.teamandroidshare.R;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView tvTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTest = (TextView) findViewById(R.id.tvTest);
+        tvTest.setText("mos");
+
     }
 }
