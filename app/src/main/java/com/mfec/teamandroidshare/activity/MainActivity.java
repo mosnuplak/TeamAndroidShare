b4b9fce90600d6ab1239c2e9f6a4976f4a5371f4
conaninw2 <se57160707@gmail,com>
2017-08-03 16:14:46 +0700

    บลาๆๆๆ

--- app/src/main/java/com/mfec/teamandroidshare/activity/MainActivity.java
+++ app/src/main/java/com/mfec/teamandroidshare/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mfec.teamandroidshare.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.widget.Toast;
 
 import com.mfec.teamandroidshare.R;
 
@@ -11,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        Toast.makeText(MainActivity.this,
+                "Width = " + width + ", Height = " + height,
+                Toast.LENGTH_SHORT)
+                .show();
     }
 }
