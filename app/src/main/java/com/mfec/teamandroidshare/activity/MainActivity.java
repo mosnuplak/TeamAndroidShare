418f4e1f47a43311256e031315eafb9dcd62eeb9
conaninw2 <se57160707@gmail,com>
2017-08-03 16:24:18 +0700

    บลาๆๆๆ ลองๆๆๆ

--- app/src/main/java/com/mfec/teamandroidshare/activity/MainActivity.java
+++ app/src/main/java/com/mfec/teamandroidshare/activity/MainActivity.java
@@ -1,10 +1,7 @@
 package com.mfec.teamandroidshare.activity;
 
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
-import android.widget.Toast;
 
 import com.mfec.teamandroidshare.R;
 
@@ -15,16 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
 
-        Toast.makeText(MainActivity.this,
-                "Width = " + width + ", Height = " + height,
-                Toast.LENGTH_SHORT)
-                .show();//555
     }
     //fah narak
+    //game เองง
 }
