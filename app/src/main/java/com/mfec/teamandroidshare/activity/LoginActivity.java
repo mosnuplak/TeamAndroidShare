55bf852f37ccabbf26476a45a877de652d7647d4
Noob Fahh <se57160689@gmail.com>
2017-08-04 15:27:58 +0700

    add  LoginActivity and activity_login.xml

--- app/src/main/java/com/mfec/teamandroidshare/activity/LoginActivity.java
+++ app/src/main/java/com/mfec/teamandroidshare/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.mfec.teamandroidshare.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.mfec.teamandroidshare.R;
 
@@ -11,5 +12,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        );
     }
 }
