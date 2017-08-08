a245764dd55abe50200f06236a9a5e9761a6e751
Noob Fahh <se57160689@gmail.com>
2017-08-08 16:15:57 +0700

    edit หน้า loginActivity.java เพิ่มคีย์ลัดบน แป้นพิมพ์

--- app/src/main/java/com/mfec/teamandroidshare/activity/LoginActivity.java
+++ app/src/main/java/com/mfec/teamandroidshare/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.mfec.teamandroidshare.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -35,7 +36,41 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void initinstances(){
         btnLogin = (Button) findViewById(R.id.btn_login);
         editUsername = (EditText) findViewById(R.id.input_username);
+        editUsername.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    switch (keyCode) {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            Intent i = new Intent(getApplication(),CategoryActivity.class);
+                            startActivity(i);
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
         editPassword = (EditText) findViewById(R.id.input_password);
+        editPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    switch (keyCode) {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            Intent i = new Intent(getApplication(),CategoryActivity.class);
+                            startActivity(i);
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
     }
     @Override
     public void onClick(View v) {
