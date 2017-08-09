029c3e4b390b9bb3e3a7c48f22b3bebb31b26dd5
conaninw2 <se57160707@gmail,com>
2017-08-09 16:00:01 +0700

    ปรับหน้าตา ปุ่มเมนู หน้า Category ใหม่

--- app/src/main/java/com/mfec/teamandroidshare/fragment/FragmentCategory.java
+++ app/src/main/java/com/mfec/teamandroidshare/fragment/FragmentCategory.java
@@ -75,8 +75,9 @@ public class FragmentCategory extends Fragment {
 
                 if (fabItem.getItemId() == fab_addCategory) {
                     Toast.makeText(getContext(),
-                            "Add "+ fabItem.getTitle(),
+                            fabItem.getTitle(),
                             Toast.LENGTH_SHORT).show();
+
                 }
                 if (fabItem.getItemId() == fab_gotoRank) {
                     Toast.makeText(
@@ -153,6 +154,23 @@ public class FragmentCategory extends Fragment {
 
         // Init 'View' instance(s) with rootView.findViewById here
     }
+//    public void doProgress(View v){
+//        ProgressDialog dialog = new ProgressDialog(getContext());
+//        dialog.setTitle("*-*");
+//        dialog.setMessage("*-*-*-*-*");
+//        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//        dialog.setIcon(R.drawable.bg);
+//        dialog.setButton("*-*-", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//
+//            }
+//        });
+//
+//
+//        dialog.show();
+//
+//    }
 
     @Override
     public void onStart() {
