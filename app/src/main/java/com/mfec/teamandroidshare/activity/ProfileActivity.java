20c6329b82b3d2c842877b457e109ef106b104e6
conaninw2 <se57160707@gmail,com>
2017-08-11 16:24:46 +0700

    เพิ่มหน้า Profile *-*

--- app/src/main/java/com/mfec/teamandroidshare/activity/ProfileActivity.java
+++ app/src/main/java/com/mfec/teamandroidshare/activity/ProfileActivity.java
@@ -1,20 +1,30 @@
 package com.mfec.teamandroidshare.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.mfec.teamandroidshare.R;
 
 import mehdi.sakout.fancybuttons.FancyButton;
 
-public class ProfileActivity extends AppCompatActivity {
-FancyButton btnGoRank;
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+    FancyButton btn_rank2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-      
+        btn_rank2 = (FancyButton) findViewById(R.id.btn_rank2);
+        btn_rank2.setOnClickListener(this);
     }
 
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(getApplication(), RankActivity.class);
+        startActivity(i);
+    }
 }
