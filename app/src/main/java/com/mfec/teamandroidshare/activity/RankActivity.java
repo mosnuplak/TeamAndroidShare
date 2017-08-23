f839a24f61edfe716d7899ec078635d649893044
moswhoami <se57160094@gmail.com>
2017-08-23 18:09:05 +0700

    ปรับหน้า Rank ให้เต็ม

--- app/src/main/java/com/mfec/teamandroidshare/activity/RankActivity.java
+++ app/src/main/java/com/mfec/teamandroidshare/activity/RankActivity.java
@@ -3,6 +3,7 @@ package com.mfec.teamandroidshare.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.mfec.teamandroidshare.R;
@@ -23,8 +24,7 @@ public class RankActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -32,22 +32,24 @@ public class RankActivity extends AppCompatActivity {
                     .commit();
         }
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//
-//        if (item.getItemId() == R.id.action_settings) {
-//            Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-//            finish();
-//            startActivity(i);
-//            return true;
-//
-//        }
-//        return super.onOptionsItemSelected(item);
-//
-//    }
-//    @Override
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home :
+                onBackPressed();
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        getMenuInflater().inflate(R.menu.menu, menu);
 //        return super.onCreateOptionsMenu(menu);
